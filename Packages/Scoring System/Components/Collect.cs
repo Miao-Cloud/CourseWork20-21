d737bcf07b1590c29ce82650b61f4fc09e5b8786
Kai <72072855+Miao-Cloud@users.noreply.github.com>
2021-02-23 13:48:22 +0000

    Add files via upload

---
+++ Packages/Scoring System/Components/Collect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collect : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        ScoringSystem.theScore += 1; //the number can be whatever you want it to be
+        Destroy(gameObject);
+    }
+}
---
+++ Packages/Scoring System/Components/Deposit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deposit : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (ScoringSystem.theScore == 5)
+        {
+            ScoringSystem.theScore -= 5; //number can be whatever you want it to be
+        }
+        
+    }
+
+}
