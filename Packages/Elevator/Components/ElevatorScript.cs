5104cb20f14fbaa87495434f7cdbfd5c5ec6866d
Kai <72072855+Miao-Cloud@users.noreply.github.com>
2021-03-16 15:41:55 +0000

    Add files via upload

---
+++ Packages/Elevator/Components/ElevatorScript.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorScript : MonoBehaviour
+{
+    public GameObject elevatorPanel;
+
+    enum EleStates{goUp, goDown, PauseState};
+    EleStates states;
+
+    public Transform top;
+    public Transform bottom;
+
+    public float smooth;
+
+    Vector3 newPos;
+    bool hasRider;
+
+    void Start()
+    {
+        elevatorPanel.SetActive(false);
+        states = EleStates.PauseState;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown (KeyCode.Q) && hasRider)
+        {
+            states = EleStates.goUp;
+        }
+
+        if (Input.GetKeyDown (KeyCode.E) && hasRider)
+        {
+            states = EleStates.goDown;
+        }
+
+        FMS();
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.tag == "Player")
+        {
+            elevatorPanel.SetActive(true);
+            coll.transform.parent = gameObject.transform;
+            hasRider = true;
+        }
+    }
+
+    void OnTriggerExit(Collider coll)
+    {
+        if (coll.tag == "Player")
+        {
+            elevatorPanel.SetActive(false);
+            coll.transform.parent = null;
+            hasRider = false;
+        }
+    }
+
+    void FMS()
+    {
+        if (states == EleStates.goDown)
+        {
+            newPos = bottom.position;
+            transform.position = Vector3.Lerp(transform.position, newPos, smooth * Time.deltaTime);
+        }
+
+        if(states == EleStates.goUp)
+        {
+            newPos = top.position;
+            transform.position = Vector3.Lerp(transform.position, newPos, smooth * Time.deltaTime);
+        }
+
+        if(states == EleStates.PauseState)
+        {
+            
+        }
+    }
+}
